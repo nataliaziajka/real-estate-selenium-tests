6bb7d4a5f8d9963457d9508098c289a302315e7d
nataliaziajka <natalia-ziajka@wp.pl>
2021-10-14 14:24:45 +0200

    Small cosmetic changes

---
+++ src/main/java/pages/KrakowFlatsPage.java
@@ -0,0 +1,81 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class KrakowFlatsPage extends BasePage {
+
+    @FindBy(css = ".rp-149nj2r > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)")
+    private WebElement flatTypeMenu;
+
+    @FindBy(xpath = "//section[1]//span[text()='Domy']")
+    private WebElement homeCheckbox;
+
+    @FindBy(css = "div.rp-1uxkh7x:nth-child(3) > button:nth-child(1)")
+    private WebElement confirmButton;
+
+    @FindBy(xpath = "//section[1]//span[text()='Pokoje']")
+    private WebElement roomMenu;
+
+    @FindBy(xpath = "//section[1]//div/span/span[text()='3']")
+    private WebElement roomNumber;
+
+    @FindBy(xpath = "//section[1]//span[text()='Powierzchnia']")
+    private WebElement flatMenu;
+
+    @FindBy(xpath = "//input[@data-testid='input-area__lower']")
+    private WebElement size_From;
+
+    @FindBy(xpath = "//input[@data-testid='input-area__upper']")
+    private WebElement size_To;
+
+    @FindBy(xpath = "//section[1]//span[text()='Cena']")
+    private WebElement priceMenu;
+
+    @FindBy(xpath = "//input[@data-testid='input-price__lower']")
+    private WebElement price_From;
+
+    @FindBy(xpath = "//input[@data-testid='input-price__upper']")
+    private WebElement price_To;
+
+    public KrakowFlatsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public KrakowFlatsPage selectFlatType() {
+        flatTypeMenu.click();
+        homeCheckbox.click();
+        confirmButton.click();
+        return this;
+    }
+
+    public KrakowFlatsPage selectRoomsNumbers() {
+        roomMenu.click();
+        roomNumber.click();
+        confirmButton.click();
+        return this;
+    }
+
+    public KrakowFlatsPage selectFlatSize() {
+        flatMenu.click();
+        size_From.click();
+        size_From.sendKeys("50");
+        size_To.click();
+        size_To.sendKeys("60");
+       return this;
+    }
+
+    public KrakowFlatsPage selectFlatPrice() {
+        priceMenu.click();
+        price_From.click();
+        price_From.sendKeys("50");
+        price_To.click();
+        price_To.sendKeys("60");
+        return this;
+    }
+
+
+
+
+}
---
+++ src/test/java/SearchingFlatsTests.java
@@ -0,0 +1,26 @@
+import lombok.val;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.KrakowFlatsPage;
+import pages.RynekPierwotnyPage;
+
+public class SearchingFlatsTests extends BaseTest {
+
+    @Test(groups = {"functest"})
+    public void findFlatsFromYourCity() {
+        val page = new RynekPierwotnyPage(driver);
+        page.waitToPageToLoad();
+        page.setSearchFieldAndConfirm("Kraków");
+        //Assertions.assertThat(page.getSearchField().getAttribute("value")).isEqualTo("Kraków");
+    }
+    @Test(dependsOnMethods = "findFlatsFromYourCity")
+    public void findFlatsWithSelectedSizeAndNumberOfRooms(){
+        val page = new KrakowFlatsPage(driver);
+        page.selectFlatType();
+        page.selectRoomsNumbers();
+        page.selectFlatSize();
+        page.selectFlatPrice();
+        //Assertions...
+    }
+}
