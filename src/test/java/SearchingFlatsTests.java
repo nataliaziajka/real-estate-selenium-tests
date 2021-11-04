4448a0409a86335ae1f096a72ecccba0c95f1ca3
nataliaziajka <natalia-ziajka@wp.pl>
2021-11-04 12:58:58 +0100

    Improve current tests

--- src/test/java/SearchingFlatsTests.java
+++ src/test/java/SearchingFlatsTests.java
@@ -1,6 +1,5 @@
 import lombok.val;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.KrakowFlatsPage;
 import pages.RynekPierwotnyPage;
@@ -9,20 +8,27 @@ public class SearchingFlatsTests extends BaseTest {
 
     @Test(groups = {"functest"})
     public void findFlatsFromYourCity() {
+        searchFlats();
+        //Assertions.assertThat(page.getSearchField().getAttribute("value")).isEqualTo("Kraków");
+    }
+
+    @Test(groups = {"functest"})
+    public void findFlatsWithSelectedSizeAndNumberOfRooms(){
+
+        val krakowFlatsPage = searchFlats();
+
+        krakowFlatsPage.waitToPageToLoad();
+        krakowFlatsPage.selectFlatType();
+        krakowFlatsPage.selectRoomsNumbers();
+        krakowFlatsPage.selectFlatSize();
+        krakowFlatsPage.selectFlatPrice();
+        //Assertions...
+    }
+
+    private KrakowFlatsPage searchFlats() {
         val page = new RynekPierwotnyPage(driver);
         page.waitToPageToLoad();
         page.setSearchFieldAndConfirm("Kraków");
-        //Assertions.assertThat(page.getSearchField().getAttribute("value")).isEqualTo("Kraków");
-    }
-    @Test(groups = {"functest"})
-    public void findFlatsWithSelectedSizeAndNumberOfRooms(){
-        val page = new KrakowFlatsPage(driver);
-        driver.get("https://rynekpierwotny.pl/s/nowe-mieszkania-i-domy-krakowski/");
-        page.waitToPageToLoad();
-        page.selectFlatType();
-        page.selectRoomsNumbers();
-        page.selectFlatSize();
-        page.selectFlatPrice();
-        //Assertions...
+        return new KrakowFlatsPage(driver);
     }
 }
